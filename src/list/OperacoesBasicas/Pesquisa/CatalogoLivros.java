22e0bb437b9f79fb4db6b5599fa08d6747085607
edycristo03 <edmilsonivair@gmail.com>
2024-12-17 17:36:43 -0300

    Add novas classes livro e catalogoLivro
    Signed-off-by: edycristo03 <edmilsonivair@gmail.com>

---
+++ src/list/OperacoesBasicas/Pesquisa/CatalogoLivros.java
@@ -0,0 +1,72 @@
+package list.OperacoesBasicas.Pesquisa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros {
+    //Atributo
+    private List<Livro> livroList;
+
+    public CatalogoLivros() {
+        this.livroList = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao){
+       livroList.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+
+    public List<Livro> pesquisarPorAutor(String autor){
+        List<Livro> livrosPorAutor = new ArrayList<>();
+        if(!livroList.isEmpty()){
+            for(Livro l : livroList){
+                if(l.getAutor().equalsIgnoreCase(autor)){
+                   livrosPorAutor.add(l);
+                }
+            }
+        }
+        return livrosPorAutor;
+    }
+
+    public List<Livro> pesquisarPorIntervaloAnos(int anoInicial, int anoFinal){
+        List<Livro> livrosPorIntervaloAnos = new ArrayList<>();
+        if(!livroList.isEmpty()){
+            for(Livro l : livroList){
+                if(l.getAnoPublicacao() >= anoInicial && l.getAnoPublicacao() <= anoFinal){
+                    livrosPorIntervaloAnos.add(l);
+                }
+            }
+        }
+        return livrosPorIntervaloAnos;
+    }
+
+    public Livro pesquisarPorTitulo(String titulo) {
+        Livro livroPorTitulo = null;
+        if (!livroList.isEmpty()) {
+            for (Livro l : livroList) {
+                if (l.getTitulo().equalsIgnoreCase(titulo)) {
+                    livroPorTitulo = l;
+                    break;
+                }
+            }
+            return livroPorTitulo;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 1", 2020);
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 2", 2019);
+        catalogoLivros.adicionarLivro("Livro 2", "Autor 2", 2021);
+        catalogoLivros.adicionarLivro("Livro 3", "Autor 3", 2022);
+        catalogoLivros.adicionarLivro("Livro 4", "Autor 4", 2023);
+
+        System.out.println(catalogoLivros.pesquisarPorAutor("Autor 2"));
+        System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2019, 2022));
+        System.out.println(catalogoLivros.pesquisarPorTitulo("Livro 1"));
+    }
+
+}
